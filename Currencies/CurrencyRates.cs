addbc46d9398958659d24cf4ad45826c60f062ec
mbk2016 <mateuszblaszczyk94@gmail.com>
2016-05-09 20:05:41 +0200

    Added app

---
+++ Currencies/CurrencyRates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currencies
+{
+    public class CurrencyRates
+    {
+        public double EUR { get; set; }
+        public double USD { get; set; }
+        public double PLN { get; set; }
+        public double GBP { get; set; }
+
+    }
+}
