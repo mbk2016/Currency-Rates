5ff9bf96ded6d0cf73c6396bdd25654fbe76f761
mbk2016 <mateuszblaszczyk94@gmail.com>
2016-06-10 20:00:59 +0200

    Added every screen resolution support
    Added progress bar to notify user about actions happening in the
    background (data loading)

--- Currencies/MainPage.xaml.cs
+++ Currencies/MainPage.xaml.cs
@@ -1,27 +1,12 @@
-﻿#define DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System;
 using System.Net.Http;
-
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
-using System.Threading;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml;
 using Windows.UI.Popups;
-using System.Net;
 using System.Net.NetworkInformation;
+
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
@@ -60,7 +45,7 @@ namespace Currencies
             Currency result = await getCurrencyRates(selectedCurrency);
             if (result == null) return;
 
-            DateTextBlock.Text = "Kursy walut dla " + selectedCurrency + " z dnia " + result.Date;
+            DateTextBlock.Text = "Kursy walut z dnia " + result.Date;
 
             if(selectedCurrency == "EUR")
             {
@@ -68,73 +53,56 @@ namespace Currencies
                                          + "PLN: " + result.Rates.PLN.ToString() + "\n"
                                          + "GBP: " + result.Rates.GBP.ToString();
             }
-
             else if(selectedCurrency == "USD")
             {
                 CurrenciesTextBlock.Text = "EUR: " + result.Rates.EUR.ToString() + "\n"
                                          + "PLN: " + result.Rates.PLN.ToString() + "\n"
                                          + "GBP: " + result.Rates.GBP.ToString();
             }
-
             else if(selectedCurrency == "PLN")
             {
                 CurrenciesTextBlock.Text = "EUR: " + result.Rates.EUR.ToString() + "\n"
                                          + "USD: " + result.Rates.USD.ToString() + "\n"
                                          + "GBP: " + result.Rates.GBP.ToString();
             }
-
             else if(selectedCurrency == "GBP")
             {
                 CurrenciesTextBlock.Text = "EUR: " + result.Rates.EUR.ToString() + "\n"
                                          + "USD: " + result.Rates.USD.ToString() + "\n"
                                          + "PLN: " + result.Rates.PLN.ToString();
             }
-            
         }
-
 
         public async Task<Currency> getCurrencyRates(string currency)
         {
             string page = "http://api.fixer.io/latest?base=" + currency;
 
-            if (CheckNet() == false)
+            if (NetworkInterface.GetIsNetworkAvailable() == false)
             {
                 MessageBox("Nie można połączyć się z serwerem.\nUpewnij się, że masz łączność z Internetem.");
                 return null;
             }
             else
             {
+                ProgressBar.Visibility = Visibility.Visible;
+                ProgressBar.IsIndeterminate = true;
                 using (HttpClient client = new HttpClient())
                 using (HttpResponseMessage response = await client.GetAsync(page))
                 using (HttpContent content = response.Content)
                 {
                     Currency result = await content.ReadAsAsync<Currency>().ConfigureAwait(false);
+                    ProgressBar.Visibility = Visibility.Collapsed;
+                    ProgressBar.IsIndeterminate = false;
                     return result;
                 }
             }
 
         }
 
-        private async void MessageBox(string message)
+        public async void MessageBox(string message)
         {
             MessageDialog MsgBox = new MessageDialog(message);
             await MsgBox.ShowAsync();
         }
-
-        private bool CheckNet()
-        {
-
-            bool status;
-            if (NetworkInterface.GetIsNetworkAvailable() == true)
-            {
-                status = true;
-            }
-            else
-            {
-                status = false;
-            }
-            return status;
-        }
-
     }
 }
